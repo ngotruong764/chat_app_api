30d74dafb87fe0f374ed78616f189e68436efb3d
ngotruong764 <truongnq.ba12-180@st.usth.edu.vn>
2024-09-22 20:49:40 +0700

    init commit

---
+++ src/test/java/com/usth/chat_app_api/ChatAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usth.chat_app_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatAppApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
