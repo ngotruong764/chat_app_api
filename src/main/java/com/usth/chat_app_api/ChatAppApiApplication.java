131d0f735a4992a7240e28b290d95aa1710fa994
Khanh221103 <pnkhanh2211@gmail.com>
2024-11-27 21:16:34 +0700

    Update dependencies and main file

--- src/main/java/com/usth/chat_app_api/ChatAppApiApplication.java
+++ src/main/java/com/usth/chat_app_api/ChatAppApiApplication.java
@@ -2,11 +2,20 @@ package com.usth.chat_app_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableScheduling
-public class ChatAppApiApplication {
+@RestController
+public class ChatAppApiApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ChatAppApiApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ChatAppApiApplication.class, args);
