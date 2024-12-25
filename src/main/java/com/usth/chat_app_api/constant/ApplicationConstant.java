034b08d50b876f655dcba58662e7ff98ea22c30d
ngotruong764 <truongnq.ba12-180@st.usth.edu.vn>
2024-12-25 14:35:22 +0700

    update s3 bucket

--- src/main/java/com/usth/chat_app_api/constant/ApplicationConstant.java
+++ src/main/java/com/usth/chat_app_api/constant/ApplicationConstant.java
@@ -4,8 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 
 public final class ApplicationConstant {
     @Value("${amazon.s3.bucket-name}")
-//    public static String AWS_BUCKET_NAME = "store-image-chat-app";
-    public static String AWS_BUCKET_NAME = "first-s3-bucket-nqt";
+    public static String AWS_BUCKET_NAME = "store-image-chat-app";
+//    public static String AWS_BUCKET_NAME = "first-s3-bucket-nqt";
     public static final String JWT_SECRET_KEY = "JWT_SECRET";
     public static final String JWT_SECRET_DEFAULT_VALUE = "asdkjashdqrbvbsdfusid237982142134";
     public static final String JWT_HEADER = "Authorization";
